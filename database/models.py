04945315909f840332476c09006ebac76f348d01
Gregory Mann <gmann1@emich.edu>
2019-02-28 21:58:31 -0500

    Add support for adding student teams and question responses

--- database/models.py
+++ database/models.py
@@ -1,3 +1,32 @@
 """
 Contains the database classes for the application
 """
+
+
+from sqlalchemy import Column, Boolean, String, Integer, Date, ForeignKeyConstraint
+from database import Base
+
+
+class StudentTeam(Base):
+    __tablename__ = 'StudentTeams'
+
+    name = Column(String, primary_key=True)
+    year = Column(Date, primary_key=True)
+    first_student = Column(String, nullable=False)
+    second_student = Column(String)
+    password = Column(String, nullable=False)
+
+
+class StudentAnswer(Base):
+    __tablename__ = 'StudentAnswers'
+
+    team_name = Column(String, primary_key=True)
+    team_year = Column(Date, primary_key=True)
+    # TODO do we need to add in a section number to know what section this is from?
+    # section = Column(Integer, nullable=False)
+    question = Column(String, primary_key=True)
+    answer = Column(String, nullable=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(['team_name', 'team_year'], ['StudentTeams.name', 'StudentTeams.year']),
+    )
