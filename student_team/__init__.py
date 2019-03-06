26cd6d6532723cddbda3b5133d46796ae1bd5ec5
Zane Pelletier <zpelleti@emich.edu>
2019-03-05 22:40:09 -0500

    generating questions with Jinja template through the backend

--- student_team/__init__.py
+++ student_team/__init__.py
@@ -4,4 +4,31 @@ student_team = Blueprint('student_team', __name__)
 
 @student_team.route('/')
 def student_team_index():
-    return render_template('student/multiple_choice.html')
+    # fake dictionary for question generation to match info retrieved from DB
+    questions = [
+        {
+            "id": 1,
+            "question": "What is the name of the unit that helps store data in a computer?",
+            "answer1": "CPU",
+            "answer2": "Input",
+            "answer3": "Memory",
+            "answer4": "Output"
+        },
+        {
+            "id": 2,
+            "question": "This provides a step-by-step procedure for performing a task.",
+            "answer1": "Keyboard",
+            "answer2": "Algorithm",
+            "answer3": "Internet",
+            "answer4": "Windows"
+        },
+        {
+            "id": 3,
+            "question": "Which one of the following is not a programming language?",
+            "answer1": "Java",
+            "answer2": "HTML",
+            "answer3": "C++",
+            "answer4": "Binary"
+        }
+    ]
+    return render_template('student/multiple_choice.html', questions=questions)
