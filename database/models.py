684d29078cbbcbb441f37c240b9f028b5d23bc6b
Anthony Morrison <amorri45@emich.edu>
2019-03-10 20:49:52 -0400

    removed column

--- database/models.py
+++ database/models.py
@@ -5,6 +5,5 @@ class iComputeTest(Base):
     __tablename__ = 'iComputeTest'
 
     question = Column(String, primary_key=True)
-    answer = Column(String, primary_key=True)
+    #answer = Column(String, primary_key=True) Not sure if we need this as the test can pull from the questions tables
     year = Column(Date)
-    
