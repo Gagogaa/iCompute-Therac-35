3877347b43ef82b6aea557678f2dbcfe9f9d5271
Anthony Morrison <amorri45@emich.edu>
2019-03-10 22:16:12 -0400

    updated questions column to SecOneQuestion and added SecTwoNThreeQuestion

--- database/models.py
+++ database/models.py
@@ -4,8 +4,9 @@ Contains the database classes for the application
 class iComputeTest(Base):
     __tablename__ = 'iComputeTest'
 
-    id = Column(Interger, primary_key=True)
-    question = Column(String)
+    id = Column(Integer, primary_key=True)
+    SecOneQuestion = Column(String)
+    SecTwoNThreeQuestion =Column(String)
     #answer = Column(String, primary_key=True) Not sure if we need this as the test can pull from the questions tablesg
     year = Column(Date)
     studentGrade = Column(String)
