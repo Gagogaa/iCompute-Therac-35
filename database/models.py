17df38207cb43230141871056239b6063e659704
Anthony Morrison <amorri45@emich.edu>
2019-03-10 22:25:14 -0400

    removed comment

--- database/models.py
+++ database/models.py
@@ -7,6 +7,5 @@ class iComputeTest(Base):
     id = Column(Integer, primary_key=True)
     SecOneQuestion = Column(String)
     SecTwoNThreeQuestion =Column(String)
-    #answer = Column(String, primary_key=True) Not sure if we need this as the test can pull from the questions tablesg
     year = Column(Date)
     studentGrade = Column(String)
