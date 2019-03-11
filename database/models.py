aae5fa876ddc68048e1b5635d468517ba9016430
Anthony Morrison <amorri45@emich.edu>
2019-03-10 20:44:48 -0400

    added test table to database

--- database/models.py
+++ database/models.py
@@ -1,3 +1,10 @@
 """
 Contains the database classes for the application
 """
+class iComputeTest(Base):
+    __tablename__ = 'iComputeTest'
+
+    question = Column(String, primary_key=True)
+    answer = Column(String, primary_key=True)
+    year = Column(Date)
+    
