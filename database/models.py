683fc6116e4b89c199ee4fcfd55b792dbbc3336d
Anthony Morrison <amorri45@emich.edu>
2019-03-10 21:50:50 -0400

    added ID and studentGrade column

--- database/models.py
+++ database/models.py
@@ -4,6 +4,8 @@ Contains the database classes for the application
 class iComputeTest(Base):
     __tablename__ = 'iComputeTest'
 
-    question = Column(String, primary_key=True)
-    #answer = Column(String, primary_key=True) Not sure if we need this as the test can pull from the questions tables
+    id = Column(Interger, primary_key=True)
+    question = Column(String)
+    #answer = Column(String, primary_key=True) Not sure if we need this as the test can pull from the questions tablesg
     year = Column(Date)
+    studentGrade = Column(String)
