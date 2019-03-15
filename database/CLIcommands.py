81d8d275299065c827520aca99de82a4ed03c4fa
Anthony Morrison <amorri45@emich.edu>
2019-03-15 18:24:54 -0400

    adding stuff to db

--- database/CLIcommands.py
+++ database/CLIcommands.py
@@ -1,32 +1,59 @@
 import click
 import database
+from flask import current_app, g
 from flask.cli import with_appcontext
 from database.models import *
+from database.__init__ import *
+
+def init_app(app):
+    #app.teardown_appcontext(close_db)
+    app.cli.add_command(testing_data)
+
+
 
 @click.command('add-testing-data')
 @with_appcontext
-
 def testing_data():
     questions =[
-     Questions(question = 'What is the name of the unit that helps store data in a computer',
-                                answer = 'CPU',
-                                is_correct = False,
+     Questions(question = 'This provides a step-by-step procedure for performing a task.',
+                                answer = 'Keyboard',
+                                is_Correct = False,
                                 section = 1),
 
-    Questions(question = 'What is the name of the unit that helps store data in a computer',
-                                answer = 'Input',
-                                is_correct = False,
+    Questions(question = 'This provides a step-by-step procedure for performing a task.',
+                                answer = 'Algorithm',
+                                is_Correct = True,
                                 section = 1),
 
-    Questions(question = 'What is the name of the unit that helps store data in a computer',
-                                answer = 'Memory',
-                                is_correct = True,
+    Questions(question = 'This provides a step-by-step procedure for performing a task.',
+                                answer = 'Internet',
+                                is_Correct = False,
                                 section = 1),
 
-    Questions(question = 'What is the name of the unit that helps store data in a computer',
-                                    answer = 'output',
-                                    is_correct = False,
-                                    section = 1)
+    Questions(question = 'This provides a step-by-step procedure for performing a task.',
+                                answer = 'Windows',
+                                is_Correct = False,
+                                section = 1),
+
+    Questions(question = 'Which one of the following is not a programming language?',
+                                answer = 'Java',
+                                is_Correct = False,
+                                section = 1),
+
+    Questions(question = 'Which one of the following is not a programming language?',
+                                answer = 'HTML',
+                                is_Correct = False,
+                                section = 1),
+
+    Questions(question = 'Which one of the following is not a programming language?',
+                                answer = 'C++',
+                                is_Correct = False,
+                                section = 1),
+
+    Questions(question = 'Which one of the following is not a programming language?',
+                                answer = 'Binary',
+                                is_Correct = True,
+                                section = 1)
                                 ]
 
     database_session.add_all(questions)
