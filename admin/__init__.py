ed5e780084661e7b7d35ba985cb07863b47d832c
0-Man <https://0-Man@github.com>
2019-03-18 11:28:46 -0400

    added info from user_create file

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 admin = Blueprint('admin', __name__, template_folder='templates')
 
 @admin.route('/', methods=('GET', 'POST'))
 def admin_index():
     return 'Hello admin'
+
+
+@admin.route('/user_create', methods=('GET', 'POST'))
+def admin_index():
+    return render_template('user_create.html')
