fb541c2bfb86509e5ce5d4633e2dc702c0fa0f02
Zane Pelletier <zpelleti@emich.edu>
2019-03-19 16:33:08 -0400

    Made it so that the cards of questions pull dynamically from the DB and added and extra card to the DB testing data

--- student_team/__init__.py
+++ student_team/__init__.py
@@ -2,46 +2,36 @@ from flask import Blueprint, render_template, request
 from database.models import *
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
 engine = create_engine('sqlite:///iCompute.db', convert_unicode=True)
-database_session = scoped_session(sessionmaker(autocommit=False,
-                                               autoflush=False,
-                                               bind=engine))
+Session = sessionmaker(bind=engine)
+session = Session()
 
 student_team = Blueprint('student_team', __name__, template_folder='templates')
 
 @student_team.route('/', methods=('GET', 'POST'))
 def student_team_index():
+
+    questions = []
+    data = {}
+    counter = 1
+    ansNum = 1
+
     # Build Dictionary for questions pulled from the db
-    questions = [
-        {
-            "id": 1,
-            "question": "What is the name of the unit that helps store data in a computer?",
-            "answer1": "CPU",
-            "answer2": "Input",
-            "answer3": "Memory",
-            "answer4": "Output"
-        },
-        {
-            "id": 2,
-            "question": "This provides a step-by-step procedure for performing a task.",
-            "answer1": "Keyboard",
-            "answer2": "Algorithm",
-            "answer3": "Internet",
-            "answer4": "Windows"
-        },
-        {
-            "id": 3,
-            "question": "Which one of the following is not a programming language?",
-            "answer1": "Java",
-            "answer2": "HTML",
-            "answer3": "C++",
-            "answer4": "Binary"
-        }
-    ]
+    for question in session.query(Questions.question).distinct():
+        data['id'] = counter
+        data['question'] = question.question
+        for answer in session.query(Questions.answer).filter(Questions.question == question.question):
+            ans = 'answer' + str(ansNum)
+            data[ans] = answer.answer
+            ansNum += 1
+        questions.append(data)
+        ansNum = 1
+        counter += 1
+        data = {}
 
     #grab the Student submitted answers off the form and submit to database
     if request.method == 'POST':
