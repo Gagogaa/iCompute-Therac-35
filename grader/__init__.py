412fa34bc929250bead9b91a300e7de9fdee74ce
Zane Pelletier <zpelleti@emich.edu>
2019-03-24 16:10:15 -0400

    Starting writing a method for automatically grading the section 1 student test results

--- grader/__init__.py
+++ grader/__init__.py
@@ -11,6 +11,15 @@ engine = create_engine('sqlite:///iCompute.db', convert_unicode=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+def grade_section_one(team):
+    correct_answers = 0
+    for answer in session.query.filter(StudentAnswer.team_name == team):
+        correct_answer = session.query.filter(_and(Questions.question == answer.question, Questions.is_correct == True))
+        if correct_answer.answer == answer.answer:
+            correct_answers += 1
+
+
+
 @grader.route('/')
 def grader_index():
     teams = []
@@ -19,5 +28,7 @@ def grader_index():
 
     if request.method == 'POST':
         team = request.form['team_name']
+        score = grade_section_one(team)
+
 
     return render_template('grader_home.html', teams=teams)
