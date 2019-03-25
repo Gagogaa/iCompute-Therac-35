be7b09785809a40f129ee29ce0d7036ff0295204
Rachel Stutzman <amorri45@emich.edu>
2019-03-25 10:09:29 -0400

    added database queries

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,71 +1,23 @@
-import click
-import database
-from flask import Blueprint
-from database.models import *
-from database.__init__ import *
+from flask import Blueprint, render_template
+
 admin = Blueprint('admin', __name__, template_folder='templates')
 
-@admin.route('/', methods=('GET', 'POST'))
+@admin.route('/')
 def admin_index():
-    return 'Hello admin'
+    return render_template('index.html', link="#", link2="./test", link3="./question", link4="./user", link5="./results")
 
-def add_questions():
-    section = 1
-    if section=1:
-        if 'question' in request.form and 'answer' in request.form :
-        question = [Questions(question = request.form['question'],'
-                                   answer = request.form['answer'],
-                                   is_correct = True,
-                                   section = 1)]
-        database_session.add(question)
-        database_session.commit()
-    elif section=2:
-        if 'question' in request.form :
-        question = [ Questions(question = request.form['question'],
-                                   answer = 'This is a section 2 question',
-                                   is_Correct = True,
-                                   section = 2)
-                                   ]
-        database_session.add(question)
-        database_session.commit()
+@admin.route('/test')
+def admin_create_test():
+    return render_template('test_create.html', link="./")
 
-    elif section=3:
-        if 'question' in request.form :
-        question = [ Questions(question = request.form['question'],
-                                    answer = 'This is a section 3 question',
-                                    is_Correct = True,
-                                    section = 3),
-                                    ]
-        database_session.add(question)
-        database_session.commit()
+@admin.route('/user')
+def admin_edit_users():
+	return render_template('userAdd.html', link="./")
 
+@admin.route('/question')
+def admin_edit_questions():
+	return render_template('questionEditUI.html', link="./")
 
-def del_question():
-    #test in CLI Form
-    if 'answer' in request.form and 'question' in request.form :
-    del_st = Questions.query.filter_by(question=request.form['question']).all()
-        database_session.delete(del_st)
-        database_session.commit()
-
-
-def del_answer():
-    #test in CLI form
-    if 'answer' in request.form and 'question' in request.form :
-    del_st = questions.query.filter(and_(answer=request.form['answer'], question=request.form['question']))
-        database_session.delete(del_st)
-        database_session.commit()
-
-def update_question():
-    #test in CLI form
-    if 'question' in request.form and 'new_question' in request.form :
-    Questions.update().values(question=request.form ['new_question']).where(
-        Questions.c.question==request.form ['question'])
-    database_session.commit()
-
-
-def update_answer():
-    #test in CLI form
-    if 'question' in request.form and 'answer' in request.form and 'new_answer' in request.form :
-    Questions.update().values(answer=request.form ['new_answer']).where(
-        Questions.c.question==request.form ['question']).and(Questions.c.answer == request.form ['answer'])
-    database_session.commit()
+@admin.route('/results')
+def admin_view_results():
+	return render_template('testResults.html', link="./")
