1399d21387c5c56d7d3ec6f715c32c20b8257bf2
joshuawilson083 <joshuawilson083@gmail.com>
2019-03-25 22:51:58 -0400

    test results pulls some data from database. Need test name implemented to complete.

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from database import database_session
+from database.models import *
 
 admin = Blueprint('admin', __name__, template_folder='templates')
 
@@ -22,20 +24,20 @@ def admin_edit_questions():
 def admin_view_results():
 
 	theScores = []
-    data = {}
-    counter = 1
+	data = {}
+	counter = 1
 
 	# Build Dictionary for questions pulled from the db
-    for testResult in database_session.query(StudentScores.team_name).distinct():
-        data['id'] = counter
-        data['teamName'] = testResult.team_name
-        data['teamYear'] = testResult.team_year
-        data['section1Score'] = testResult.section_one_score
-        data['section2Score'] = testResult.section_two_score
-        data['section3Score'] = testResult.section_three_score
-        data['section4Score'] = testResult.total_score
-        
-        theScores.append(data)
+	for testResult in database_session.query(StudentScore).distinct():
+		data['id'] = counter
+		data['teamName'] = testResult.team_name
+		data['teamYear'] = testResult.team_year
+		data['section1Score'] = testResult.section_one_score
+		data['section2Score'] = testResult.section_two_score
+		data['section3Score'] = testResult.section_three_score
+		data['section4Score'] = testResult.total_score
+
+		theScores.append(data)
 		counter += 1
 		data = {}
 
