9805bb3beb89ac20bf6aa29132280ed5139065be
Zane Pelletier <zpelleti@emich.edu>
2019-03-26 20:28:16 -0400

    Started working on the test creation page

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import *
 
 admin = Blueprint('admin', __name__, template_folder='admin_templates')
 
@@ -8,7 +8,19 @@ def admin_index():
 
 @admin.route('/test')
 def admin_modify_test():
-    return render_template('test_modify.html', link="./")
+    return render_template('test_modify.html', link=url_for('admin.admin_create_test'), link2=url_for('admin.admin_edit_test'), link3=url_for('admin.admin_view_test'))
+
+@admin.route('test/test_create')
+def admin_create_test():
+    return render_template('test_create.html')
+
+@admin.route('test/test_edit')
+def admin_edit_test():
+    return render_template('test_edit.html')
+
+@admin.route('test/test_view')
+def admin_view_test():
+    return render_template('test_view.html')
 
 @admin.route('/user')
 def admin_edit_users():
